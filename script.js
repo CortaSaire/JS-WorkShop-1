a7d78ab9a6ab202d9ee08e9325fb39658acdebbd
CortaSaire <cortasaire@gmail.com>
2024-10-27 22:52:53 +0400

    Rac Minda

---
+++ script.js
@@ -0,0 +1,31 @@
+let name = "John"; // String // არ შეიძლება რედეკლარირება
+var userIsLoggedIn = false; // Boolean // არ შეიძლება რედეკლარირება
+
+const birthDay = 1997; //Number // არ შეიძლება რედეკლარირება
+var surname = "Doe"; //
+
+var userIsValid = name; //Undefined
+
+name = userIsValid;
+
+let data;
+
+data = name;
+
+// console.log(data); // John
+
+// --- // Data types in JS
+let string = "Gondor"; // String
+let number = 100; //Number
+let usrIsLoggedIn = false; // Boolean
+let BigInt = 9007199254740991n; // Bigint
+let UNDEFINED = undefined; // undefined
+let nullType = null; //null
+
+// --- // Basic operators, maths
+let concat = string + usrIsLoggedIn; // კონკატენაცია;
+let nullIs0 = number - nullType; // Null გადააქცევს 0
+let nashti = number % -11; // აბრუნებს ნაშთს
+console.log(number);
+number++;
+console.log(number);
